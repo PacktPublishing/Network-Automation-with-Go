3667946cb66c93af2d3d90c4e2a370673f8dff0a
nleiva <nicolasleiva@gmail.com>
2021-12-10 15:13:35 -0500

    Create folders for udp-ping

--- ch04/udp-ping/client/main.go
+++ ch04/udp-ping/client/main.go
@@ -6,8 +6,37 @@ import (
 	"flag"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+var (
+	listenAddr     = "0.0.0.0"
+	listenPort     = 32767
+	probeSizeBytes = 9
+	maxReadBuffer  = 425984
+	retryTimeout   = time.Second * 5
+	probeInterval  = time.Second
+)
+
+func setupSigHandlers(cancel context.CancelFunc) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+
+	go func() {
+		sig := <-sigs
+		log.Printf("Received syscall: %+v", sig)
+		cancel()
+	}()
+
+}
+
+type probe struct {
+	SeqNum uint8
+	SendTS int64
+}
 
 func receive(udpConn net.UDPConn) {
 	log.Printf("Starting UDP ping receive loop")
--- ch04/udp-ping/server/main.go
+++ ch04/udp-ping/server/main.go
@@ -5,8 +5,37 @@ import (
 	"flag"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+var (
+	listenAddr     = "0.0.0.0"
+	listenPort     = 32767
+	probeSizeBytes = 9
+	maxReadBuffer  = 425984
+	retryTimeout   = time.Second * 5
+	probeInterval  = time.Second
+)
+
+func setupSigHandlers(cancel context.CancelFunc) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+
+	go func() {
+		sig := <-sigs
+		log.Printf("Received syscall: %+v", sig)
+		cancel()
+	}()
+
+}
+
+type probe struct {
+	SeqNum uint8
+	SendTS int64
+}
 
 func main() {
 	port := flag.Int("port", listenPort, "UDP listen port")
