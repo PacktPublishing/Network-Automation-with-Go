13ae23436ff4bfa14015be66c45171d30275e6e6
networkop <mmkashin@gmail.com>
2022-03-15 21:31:41 +0000

    relocation updates

--- ch10/targets.mk
+++ ch10/targets.mk
@@ -13,9 +13,9 @@ bgp-ping-stop:
 	cd ch10/bgp-ping; docker-compose down; cd ../../
 
 bgp-ping-start: bgp-ping-build
-	docker exec -d clab-netgo-host-3 /workdir/bgp-ping/bgp-ping -id host-3 -nlri 100.64.0.2 -laddr 203.0.113.254 -raddr 203.0.113.129 -las 65005 -ras 65002 -p
-	docker exec -d clab-netgo-host-1 /workdir/bgp-ping/bgp-ping -id host-1 -nlri 100.64.0.0 -laddr 203.0.113.0 -raddr 203.0.113.1 -las 65003 -ras 65000 -p
-	docker exec -d clab-netgo-host-2 /cloudprober -config_file /workdir/workdir/cloudprober.cfg
+	docker exec -d clab-netgo-host-3 /workdir/ch10/bgp-ping/bgp-ping -id host-3 -nlri 100.64.0.2 -laddr 203.0.113.254 -raddr 203.0.113.129 -las 65005 -ras 65002 -p
+	docker exec -d clab-netgo-host-1 /workdir/ch10/bgp-ping/bgp-ping -id host-1 -nlri 100.64.0.0 -laddr 203.0.113.0 -raddr 203.0.113.1 -las 65003 -ras 65000 -p
+	docker exec -d clab-netgo-host-2 /cloudprober -config_file /workdir/topo-full/workdir/cloudprober.cfg
 	cd ch10/bgp-ping; docker-compose up -d; cd ../../
 	@echo 'http://localhost:3000'
 
@@ -37,8 +37,8 @@ top-talkers-start: traffic-start
 
 capture-start: traffic-start
 	cd ch10/packet-capture; go build -o packet-capture main.go
-	docker exec -it clab-netgo-host-2 /workdir/packet-capture/packet-capture
+	docker exec -it clab-netgo-host-2 /workdir/ch10/packet-capture/packet-capture
 
 capture-debug:
-	echo "docker exec -it clab-netgo-host-2 bash -c 'cd /workdir/packet-capture/; dlv debug main.go --listen=:2345 --headless --api-version=2'"
+	echo "docker exec -it clab-netgo-host-2 bash -c 'cd /workdir/ch10/packet-capture/; dlv debug main.go --listen=:2345 --headless --api-version=2'"
 
