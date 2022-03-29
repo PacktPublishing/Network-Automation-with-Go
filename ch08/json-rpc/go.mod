ac740181c37fa6012a767d26ff78283a0c78f261
networkop <mmkashin@gmail.com>
2022-03-29 22:15:03 +0100

    json-rpc almost done

--- ch08/json-rpc/go.mod
+++ ch08/json-rpc/go.mod
@@ -2,10 +2,7 @@ module json-rpc
 
 go 1.17
 
-require (
-	github.com/openconfig/goyang v1.0.0
-	github.com/openconfig/ygot v0.16.2
-)
+require github.com/openconfig/ygot v0.16.2
 
 require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
@@ -13,6 +10,7 @@ require (
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/openconfig/gnmi v0.0.0-20200508230933-d19cebf5e7be // indirect
+	github.com/openconfig/goyang v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
 	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
 	golang.org/x/text v0.3.6 // indirect
--- ch08/json-rpc/main.go
+++ ch08/json-rpc/main.go
@@ -7,14 +7,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"json-rpc/pkg/srl"
+	api "json-rpc/pkg/srl"
 	"log"
 	"net/http"
 
 	"github.com/openconfig/ygot/ygot"
 )
 
-//go:generate go run github.com/openconfig/ygot/generator -path=yang -output_file=pkg/srl/srl.go -package_name=srl yang/interfaces/srl_nokia-interfaces.yang yang/interfaces/srl_nokia-if-ip.yang
+// docker exec -it clab-netgo-srl /opt/srlinux/bin/sr_cli
+
+//go:generate go run github.com/openconfig/ygot/generator -path=yang -output_file=pkg/srl/srl.go -package_name=srl yang/srl_nokia/models/network-instance/srl_nokia-bgp.yang yang/srl_nokia/models/routing-policy/srl_nokia-routing-policy.yang
 
 var (
 	hostname = "http://clab-netgo-srl/jsonrpc"
@@ -30,7 +32,7 @@ type RPC struct {
 }
 
 type Params struct {
-	Commands []Command `json:"commands"`
+	Commands []*Command `json:"commands"`
 }
 
 type Command struct {
@@ -40,31 +42,129 @@ type Command struct {
 }
 
 func buildL3Interface(name, prefix string) (*Command, error) {
-	intf := srl.SrlNokiaInterfaces_Interface{}
+	intf := api.SrlNokiaInterfaces_Interface{}
 	subintf, err := intf.NewSubinterface(0)
 	if err != nil {
 		return nil, err
 	}
 
-	subintf.Ipv4 = &srl.SrlNokiaInterfaces_Interface_Subinterface_Ipv4{}
+	subintf.Ipv4 = &api.SrlNokiaInterfaces_Interface_Subinterface_Ipv4{}
 	subintf.Ipv4.NewAddress(prefix)
 
 	if err := intf.Validate(); err != nil {
 		return nil, err
 	}
 
-	ietf, err := ygot.ConstructIETFJSON(&intf, nil)
+	value, err := ygot.ConstructIETFJSON(&intf, nil)
 	if err != nil {
 		return nil, err
 	}
+
+	fmt.Printf("\n/interface[name=%s]>\n", name)
+	printYgot(&intf)
+
 	return &Command{
 		Action: "replace",
 		Path:   fmt.Sprintf("/interface[name=%s]", name),
-		Value:  ietf,
+		Value:  value,
 	}, nil
 }
 
-func buildSetRPC(cmds []Command) RPC {
+func buildBGPConfig(instance, id, group, policy, neighbor string, lasn, rasn uint32) (*Command, error) {
+
+	bgp := &api.SrlNokiaNetworkInstance_NetworkInstance_Protocols_Bgp{
+		AutonomousSystem: uintToPtr(lasn),
+		RouterId:         strToPtr(id),
+		Ipv4Unicast: &api.SrlNokiaNetworkInstance_NetworkInstance_Protocols_Bgp_Ipv4Unicast{
+			AdminState: api.SrlNokiaBgp_AdminState_enable,
+		},
+	}
+
+	g, err := bgp.NewGroup(group)
+	if err != nil {
+		return nil, err
+	}
+	g.ExportPolicy = strToPtr(policy)
+	g.ImportPolicy = strToPtr(policy)
+
+	n, err := bgp.NewNeighbor(neighbor)
+	if err != nil {
+		return nil, err
+	}
+	n.PeerAs = uintToPtr(rasn)
+	n.PeerGroup = strToPtr(group)
+
+	if err := bgp.Validate(); err != nil {
+		return nil, err
+	}
+
+	value, err := ygot.ConstructIETFJSON(bgp, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("\n/network-instance[name=%s]/protocols/bgp>\n", instance)
+	printYgot(bgp)
+
+	return &Command{
+		Action: "update",
+		Path:   fmt.Sprintf("/network-instance[name=%s]/protocols/bgp", instance),
+		Value:  value,
+	}, nil
+
+}
+
+func moveIntfsToInstance(intfs []string, instance string) ([]*Command, error) {
+	var cmds []*Command
+	for _, intf := range intfs {
+		path := fmt.Sprintf("/network-instance[name=%s]/interface[name=%s]", instance, intf)
+		fmt.Println(path)
+		cmds = append(cmds, &Command{
+			Action: "update",
+			Path:   path,
+		})
+	}
+
+	return cmds, nil
+}
+
+func buildDefaultPolicy(name string) (*Command, error) {
+	rp := api.SrlNokiaRoutingPolicy_RoutingPolicy{}
+	p, err := rp.NewPolicy(name)
+	if err != nil {
+		return nil, err
+	}
+
+	// populating LocalPreference due to YGOT not supporting presence containers, see "ygot/issues/329"
+	p.DefaultAction = &api.SrlNokiaRoutingPolicy_RoutingPolicy_Policy_DefaultAction{
+		Accept: &api.SrlNokiaRoutingPolicy_RoutingPolicy_Policy_DefaultAction_Accept{
+			Bgp: &api.SrlNokiaRoutingPolicy_RoutingPolicy_Policy_DefaultAction_Accept_Bgp{
+				LocalPreference: &api.SrlNokiaRoutingPolicy_RoutingPolicy_Policy_DefaultAction_Accept_Bgp_LocalPreference{
+					Set: uintToPtr(uint32(100)),
+				},
+			},
+		},
+	}
+
+	if err := rp.Validate(); err != nil {
+		return nil, err
+	}
+	value, err := ygot.ConstructIETFJSON(&rp, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("\n/routing-policy>\n")
+	printYgot(&rp)
+
+	return &Command{
+		Action: "replace",
+		Path:   "/routing-policy",
+		Value:  value,
+	}, nil
+}
+
+func buildSetRPC(cmds []*Command) RPC {
 	return RPC{
 		Version: "2.0",
 		ID:      0,
@@ -75,21 +175,51 @@ func buildSetRPC(cmds []Command) RPC {
 	}
 }
 
+func printYgot(s ygot.ValidatedGoStruct) {
+	t, _ := ygot.EmitJSON(s, &ygot.EmitJSONConfig{
+		Format: ygot.Internal,
+		Indent: "  ",
+		RFC7951Config: &ygot.RFC7951JSONConfig{
+			AppendModuleName: true,
+		},
+	},
+	)
+	fmt.Println(t)
+}
+
 func main() {
 
-	var cmds []Command
+	var cmds []*Command
 
 	eth1, err := buildL3Interface("ethernet-1/1", "192.0.2.0/31")
 	if err != nil {
 		log.Fatal(err)
 	}
-	cmds = append(cmds, *eth1)
+	cmds = append(cmds, eth1)
 
 	lo0, err := buildL3Interface("system0", "198.51.100.0/32")
 	if err != nil {
 		log.Fatal(err)
 	}
-	cmds = append(cmds, *lo0)
+	cmds = append(cmds, lo0)
+
+	policy, err := buildDefaultPolicy("all")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmds = append(cmds, policy)
+
+	insts, err := moveIntfsToInstance([]string{"ethernet-1/1.0", "system0.0"}, "default")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmds = append(cmds, insts...)
+
+	bgp, err := buildBGPConfig("default", "198.51.100.0", "EBGP", "all", "192.0.2.1", uint32(65000), uint32(65001))
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmds = append(cmds, bgp)
 
 	value, _ := json.Marshal(buildSetRPC(cmds))
 
@@ -104,6 +234,9 @@ func main() {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}}
 	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if resp.StatusCode != http.StatusOK {
 		log.Printf("Status: %s", resp.Status)
@@ -117,3 +250,6 @@ func main() {
 	fmt.Println(string(body))
 
 }
+
+func strToPtr(v string) *string  { return &v }
+func uintToPtr(v uint32) *uint32 { return &v }
