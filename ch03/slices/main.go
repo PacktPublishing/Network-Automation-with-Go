e1d6b841fba6a76c046ec223bc1744707377e074
nleiva <nicolasleiva@gmail.com>
2022-05-02 15:46:04 -0400

    Fix slice bug pointed out by Chris

--- ch03/slices/main.go
+++ ch03/slices/main.go
@@ -10,18 +10,18 @@ func main() {
 	three := make([]string, 3)
 
 	fmt.Printf("empty: length: %d, capacity: %d, %v\n", len(empty), cap(empty), empty)
-	fmt.Printf("empty: length: %d, capacity: %d, %v\n", len(words), cap(words), words)
-	fmt.Printf("empty: length: %d, capacity: %d, %v\n", len(three), cap(three), three)
+	fmt.Printf("words: length: %d, capacity: %d, %v\n", len(words), cap(words), words)
+	fmt.Printf("three: length: %d, capacity: %d, %v\n", len(three), cap(three), three)
 
 	// Creating a slice
 	mySlice := words[1:3]
-	fmt.Printf("empty: length: %d, capacity: %d, %v\n", len(mySlice), cap(mySlice), mySlice)
+	fmt.Printf("mySlice: length: %d, capacity: %d, %v\n", len(mySlice), cap(mySlice), mySlice)
 
 	mySlice = append(mySlice, "seven")
-	fmt.Printf("empty: length: %d, capacity: %d, %v\n", len(mySlice), cap(mySlice), mySlice)
+	fmt.Printf("mySlice: length: %d, capacity: %d, %v\n", len(mySlice), cap(mySlice), mySlice)
 
 	mySlice = append(mySlice, "eight", "nine", "ten", "eleven")
-	fmt.Printf("empty: length: %d, capacity: %d, %v\n", len(mySlice), cap(mySlice), mySlice)
+	fmt.Printf("mySlice: length: %d, capacity: %d, %v\n", len(mySlice), cap(mySlice), mySlice)
 
 	
 }
