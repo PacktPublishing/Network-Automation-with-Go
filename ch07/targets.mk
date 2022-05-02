b93d6030ec3c79625b02e3dbf863ac84fc65c6ff
nleiva <nicolasleiva@gmail.com>
2022-05-02 13:07:12 -0400

    Create ch07 ansible folder

--- ch07/targets.mk
+++ ch07/targets.mk
@@ -1,11 +1,11 @@
 ## Build binary for CVX
 build-go-cvx:
-	cd ch07/cvx; go build -o ../library/go_cvx
+	cd ch07/ansible/cvx; go build -o ../library/go_cvx
 
 ## Build binary for SRL
 build-go-srl:
-	cd ch07/srl; go build -o ../library/go_srl
+	cd ch07/ansible/srl; go build -o ../library/go_srl
 
 ## Build binary for state validation
 build-go-state:
-	cd ch07/state; go build -o ../library/go_state
+	cd ch07/ansible/state; go build -o ../library/go_state
