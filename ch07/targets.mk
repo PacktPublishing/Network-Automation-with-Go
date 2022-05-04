b369185df797eca3f1ccd5cf1e33566e10d9c1f3
nleiva <nicolasleiva@gmail.com>
2022-05-04 12:57:02 -0400

    Clean up module routing

--- ch07/targets.mk
+++ ch07/targets.mk
@@ -1,15 +1,3 @@
 ## Build binary for CVX
 build-go-cvx:
 	cd ch07/ansible/cvx; go build -o ../library/go_cvx
-
-## Build binary for SRL
-build-go-srl:
-	cd ch07/ansible/srl; go build -o ../library/go_srl
-
-## Build binary for EOS
-build-go-eos:
-	cd ch07/ansible/eos; go build -o ../library/go_eos
-
-## Build binary for state validation
-build-go-state:
-	cd ch07/ansible/state; go build -o ../library/go_state
